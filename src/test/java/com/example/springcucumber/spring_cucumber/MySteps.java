ba4ee4133cecc685071c04fb97a849ff6eff493c
Vyacheslav <ffgeniy@mail.ru>
2020-02-25 22:51:30 +0300

    Refactoring harcode, add implicitywait

--- src/test/java/com/example/springcucumber/spring_cucumber/MySteps.java
+++ src/test/java/com/example/springcucumber/spring_cucumber/MySteps.java
@@ -1,26 +1,31 @@
 package com.example.springcucumber.spring_cucumber;
 
-import com.example.springcucumber.spring_cucumber.dao.UserDao;
 import com.example.springcucumber.spring_cucumber.model.Role;
 import com.example.springcucumber.spring_cucumber.model.User;
 import com.example.springcucumber.spring_cucumber.service.UserService;
 import cucumber.api.PendingException;
 import cucumber.api.java.ru.Допустим;
 import cucumber.api.java.ru.Если;
-import cucumber.api.java.ru.То;
 import cucumber.api.java.ru.Тогда;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.concurrent.TimeUnit;
+
 @ContextConfiguration(classes = SpringCucumberApplication.class)
+@PropertySource("classpath:application.properties")
 @RunWith(SpringJUnit4ClassRunner.class)
 public class MySteps {
     @Autowired
     private UserService userService;
+    @Autowired
+    private Environment env;
     private WebDriver driver = RunAddUserTest.driver;
 
     @Допустим("^мы авторизовались под админом$")
@@ -29,13 +34,9 @@ public class MySteps {
         try {
             driver.get("http://localhost:8080/logout");
             driver.get("http://localhost:8080/login");
-            System.out.println("Open URL");
-
-            driver.findElement(By.name("username")).sendKeys("admin");
-            driver.findElement(By.name("password")).sendKeys("admin");
+            driver.findElement(By.name("username")).sendKeys(env.getRequiredProperty("db.default.admin.login"));
+            driver.findElement(By.name("password")).sendKeys(env.getRequiredProperty("db.default.admin.password"));
             driver.findElement(By.className("btn")).click();
-
-            System.out.println("Successful connection with admin role!");
         } catch (Exception e) {
             e.printStackTrace();
             throw new PendingException();
@@ -53,62 +54,57 @@ public class MySteps {
         }
     }
 
-    @Если("^на сервере не существует такого же логина$")
-    public void на_сервере_не_существует_такого_же_логина() throws Throwable {
+    @Если("^на сервере не существует такого же логина \"([^\"]*)\"$")
+    public void на_сервере_не_существует_такого_же_логина(String arg1) throws Throwable {
         // Write code here that turns the phrase above into concrete actions
         try{
-            User user = userService.getUserByLogin("Огурчик");
-            if (user.getLogin().equals("")) {
-                System.out.println("Unique login");
-            } else {
+            User user = userService.getUserByLogin(arg1);
+            if (!user.getLogin().equals("")) {
                 userService.deleteUser(user.getId().toString());
             }
-        } catch (Exception e) {
-            throw new PendingException();
-        }
-    }
-
-    @Тогда("^пользователь добавляется корретно$")
-    public void пользователь_добавляется_корретно() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        try {
-            driver.findElement(By.id("navAddForm")).click();
-            Thread.currentThread().sleep(200);
-            driver.findElement(By.id("addFirstName")).sendKeys("Огурчик");
-            driver.findElement(By.id("addLastName")).sendKeys("Пестик");
-            driver.findElement(By.id("addPhoneNumber")).sendKeys("999");
-            driver.findElement(By.id("addLogin")).sendKeys("Огурчик");
-            driver.findElement(By.id("addPassword")).sendKeys("Пестик");
-            driver.findElement(By.id("butAddUser")).submit();
-            Thread.currentThread().sleep(200);
-            driver.findElement(By.id("navLinkTable")).click();
-
-            User user = userService.getUserByLogin("Огурчик");
-
-            if (user.getLogin().equals("")) {
-                System.out.println("Exception in add user with unique login");
-                throw new Exception("Exception in add user with unique login");
-            }
-
-            System.out.println("Success added user");
         } catch (Exception e) {
             e.printStackTrace();
             throw new PendingException();
         }
     }
 
-    @Если("^на сервере существует такой же логин$")
-    public void на_сервере_существует_такой_же_логин() throws Throwable {
+    @Тогда("^пользователь с логином \"([^\"]*)\" и паролем \"([^\"]*)\" добавляется корретно$")
+    public void пользователь_с_логином_и_паролем_добавляется_корретно(String arg1, String arg2) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        try {
+            driver.findElement(By.id("navAddForm")).click();
+            driver.manage().timeouts().implicitlyWait(200, TimeUnit.MILLISECONDS);
+            driver.findElement(By.id("addFirstName")).sendKeys(arg1);
+            driver.findElement(By.id("addLastName")).sendKeys(arg2);
+            driver.findElement(By.id("addPhoneNumber")).sendKeys("999");
+            driver.findElement(By.id("addLogin")).sendKeys(arg1);
+            driver.findElement(By.id("addPassword")).sendKeys(arg2);
+            driver.findElement(By.id("butAddUser")).submit();
+            //working only thread sleep
+            Thread.currentThread().sleep(200);
+            driver.findElement(By.id("navLinkTable")).click();
+
+            User user = userService.getUserByLogin(arg1);
+
+            if (user.getLogin().equals("")) {
+                throw new Exception("Exception in add user with unique login");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new PendingException();
+        }
+    }
+
+    @Если("^на сервере существует такой же логин \"([^\"]*)\" с паролем \"([^\"]*)\"$")
+    public void на_сервере_существует_такой_же_логин_с_паролем(String arg1, String arg2) throws Throwable {
         // Write code here that turns the phrase above into concrete actions
         try{
-            if (userService.getUserByLogin("Огурчик").getLogin().equals("Огурчик")) {
-                System.out.println("Double login");
-            } else {
+            if (!userService.getUserByLogin(arg1).getLogin().equals(arg1)) {
                 User user = new User(
-                        "Огурчик",
-                        "Перчик",
-                        "Огурчик",
-                        "Перчик",
+                        arg1,
+                        arg2,
+                        arg1,
+                        arg2,
                         999L,
                         new Role("admin")
                         );
@@ -119,22 +115,18 @@ public class MySteps {
         }
     }
 
-
-    @Тогда("^пользователь не добавляется$")
-    public void пользователь_не_добавляется() throws Throwable {
+    @Тогда("^пользователь с логином \"([^\"]*)\" и паролем \"([^\"]*)\" не добавляется$")
+    public void пользователь_с_логином_и_паролем_не_добавляется(String arg1, String arg2) throws Throwable {
         // Write code here that turns the phrase above into concrete actions
         try{
             driver.findElement(By.id("navAddForm")).click();
-            Thread.currentThread().sleep(200);
-            driver.findElement(By.id("addFirstName")).sendKeys("Огурчик");
-            driver.findElement(By.id("addLastName")).sendKeys("Пестик");
+            driver.manage().timeouts().implicitlyWait(200, TimeUnit.MILLISECONDS);
+            driver.findElement(By.id("addFirstName")).sendKeys(arg1);
+            driver.findElement(By.id("addLastName")).sendKeys(arg2);
             driver.findElement(By.id("addPhoneNumber")).sendKeys("999");
-            driver.findElement(By.id("addLogin")).sendKeys("Огурчик");
-            driver.findElement(By.id("addPassword")).sendKeys("Пестик");
+            driver.findElement(By.id("addLogin")).sendKeys(arg1);
+            driver.findElement(By.id("addPassword")).sendKeys(arg2);
             driver.findElement(By.id("butAddUser")).submit();
-            Thread.currentThread().sleep(200);
-
-            System.out.println("User didn't add");
         } catch (Exception e) {
             e.printStackTrace();
             throw new PendingException();
